92836712e18f9f11b1b174e9bc6d8c0a3e3a1d24
Nicolopezc <Nico.lopezc@alumnos.duoc.cl>
2020-11-23 23:35:35 -0300

    Requerimientos Funcionales Admin

---
+++ UnrealFutbol/Registro/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('registro/',views.registro, name='registro'),
+    path('terminos/',views.terminos,name='terminos'),
+    path('galeria/', views.galeria, name='galeria'),
+    path('apuesta/', views.apuesta, name='apuesta'),
+    path('usuario/<int:pk>', views.UsuarioDetailView.as_view(), name='usuario-detail'),
+    path('partidos/',views.PartidoListView.as_view(), name='partidos'),
+    path('partidos/<int:pk>', views.PartidoDetailView.as_view(), name='partido-detail'),
+]
+
+urlpatterns+=[
+    path('usuario/create/', views.UsuarioCreate.as_view(), name='usuario_create'),
+    path('usuario/<int:pk>/update/', views.UsuarioUpdate.as_view(), name='usuario_update'),
+    path('usuario/<int:pk>/delete/', views.UsuarioDelete.as_view(), name='usuario_delete'),
+    path('partido/create/', views.PartidoCreate.as_view(), name='partido_create'),
+    path('partido/<int:pk>/update/', views.PartidoUpdate.as_view(), name='partido_update'),
+    path('partido/<int:pk>/delete/', views.PartidoDelete.as_view(), name='partido_delete'),
+
+]
---
+++ UnrealFutbol/Registro/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render
+from . models import Usuario, Partido
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.urls import reverse
+
+def index(request):
+
+    return render(
+        request,
+        'index.html',
+    )
+
+def login(request):
+
+    return render(
+        request,
+        'login.html',
+    ) 
+
+def registro(request):
+
+    return render(
+        request,
+        'registro.html',
+    )  
+
+def terminos(request):
+
+    return render(
+        request,
+        'terminos.html',
+    ) 
+def galeria(request):
+
+    return render(
+        request,
+        'galeria.html',
+    )
+
+def apuesta(request):
+
+    return render(
+        request,
+        'apuesta.html',
+    )             
+
+class UsuarioCreate(CreateView):
+    model = Usuario
+    fields = '__all__'
+
+class UsuarioUpdate(UpdateView):
+    model = Usuario
+    fields = '__all__'
+
+class UsuarioDelete(DeleteView):
+    model = Usuario
+    def get_success_url(self):
+        return reverse('index')
+
+class UsuarioListView(generic.ListView):
+    model = Usuario
+
+class UsuarioDetailView(generic.DetailView):
+    model=Usuario
+
+class PartidoCreate(CreateView):
+    model = Partido
+    fields = '__all__'
+
+class PartidoUpdate(UpdateView):
+    model = Partido
+    fields = '__all__'
+
+class PartidoDelete(DeleteView):
+    model = Partido
+    def get_success_url(self):
+        return reverse('index')
+
+class PartidoListView(generic.ListView):
+    model = Partido
+
+class PartidoDetailView(generic.DetailView):
+    model = Partido
